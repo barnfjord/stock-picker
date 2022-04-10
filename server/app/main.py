de37f235701bdbd2a3d4ee1311d99a647d0d1ae2
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-04-09 22:28:22 -0600

    Static tickers for temp PoC

--- server/app/main.py
+++ server/app/main.py
@@ -2,37 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
+from app.stock_handler import get_last_prices
+
 app = FastAPI()
+
+tickers = ["AAPL", "MSFT", "GOOG", "FB", "NFLX", "TSLA", "AMZN", "SPY", "SQ", "SHOP"]
 
 
 @app.get("/picks")
 async def get_picks():
-    return JSONResponse(
-        [
-            {
-                "ticker": "AAPL",
-                "price": "120.35",
-                "change": "-0.01",
-                "ticks": [
-                    {"time": "2018-01-01T00:00:00.000Z", "price": "120.35"},
-                    {"time": "2018-01-01T00:01:00.000Z", "price": "120.35"},
-                    {"time": "2018-01-01T00:02:00.000Z", "price": "120.35"},
-                    {"time": "2018-01-01T00:03:00.000Z", "price": "120.35"},
-                ],
-            },
-            {
-                "ticker": "MSFT",
-                "price": "51.15",
-                "change": "-0.01",
-                "ticks": [
-                    {"time": "2018-01-01T00:00:00.000Z", "price": "51.15"},
-                    {"time": "2018-01-01T00:01:00.000Z", "price": "51.15"},
-                    {"time": "2018-01-01T00:02:00.000Z", "price": "51.15"},
-                    {"time": "2018-01-01T00:03:00.000Z", "price": "51.15"},
-                ],
-            },
-        ]
-    )
+    data = get_last_prices(tickers)
+    return JSONResponse(data)
 
 
 app.mount("/", StaticFiles(directory="../client/build/", html=True), name="build")
