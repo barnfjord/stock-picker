43c76a424a86a848d2e54bea1b4d184e197d1796
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-04-09 22:39:17 -0600

    Add middleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
 from app.stock_handler import get_last_prices
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 tickers = ["AAPL", "MSFT", "GOOG", "FB", "NFLX", "TSLA", "AMZN", "SPY", "SQ", "SHOP"]
 
